ad88996e3af7157ea1c568d21f4d3d1d5f2ae6d4
Jakub Gause <gause.jakub@gmail.com>
2026-01-29 15:07:47 +0100

    feat: add default_project_id config option

--- config/lettr.php
+++ config/lettr.php
@@ -14,4 +14,17 @@ return [
 
     'api_key' => env('LETTR_API_KEY'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Default Project ID
+    |--------------------------------------------------------------------------
+    |
+    | The default project ID to use when listing or fetching templates.
+    | This can be overridden by passing an explicit project ID to the
+    | template service methods.
+    |
+    */
+
+    'default_project_id' => env('LETTR_DEFAULT_PROJECT_ID'),
+
 ];
